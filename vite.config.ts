b244efebf36f86b50dd70565b002f371b35abd85
ChillTheCode <edwin.daniel@ui.ac.id>
2025-05-28 10:03:47 +0700

    fixForgeFit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/chillthecode.github.io/forgefit.github.io/',
+   base: '/chillthecode.github.io/forgefit.github.io',
 })
 
